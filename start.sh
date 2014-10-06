842187dc5f54678516825410855313e3b658f091
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-05 23:09:28 -0400

    update start script

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ADMIN="admin@${FQDN-example.org}"
-PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+if [[ -n $GENERATE_ADMIN_USER ]]; then
+  ADMIN="admin@${FQDN-example.org}"
+  PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-/opt/strider/node_modules/strider/bin/strider addUser \
-  --email $ADMIN --password $PASSWD --admin true
+  /opt/strider/node_modules/strider/bin/strider addUser \
+    --email $ADMIN --password $PASSWD --admin true
 
-echo "Admin User: $ADMIN, Admin Pass: $PASSWD"
+  echo "Admin User: $ADMIN, Admin Pass: $PASSWD"
+fi
 
 /opt/strider/node_modules/strider/bin/strider
